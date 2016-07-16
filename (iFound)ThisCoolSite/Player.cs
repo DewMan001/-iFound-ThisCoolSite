65d3506f3c80bb2157731fdbfae0cac4a6e79a27
DewMan001 <deweymanster@gmail.com>
2016-07-16 21:48:01 +0800

    Created player class

---
+++ (iFound)ThisCoolSite/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _iFound_ThisCoolSite
+{
+    class Player
+    {
+        string PlayerName;
+        int PlayerWallet;
+        Player()
+        {
+            PlayerName = "Betty Betterson";
+            PlayerWallet = 4844;
+        }
+
+        private void setPlayerName(string Name)
+        {
+            PlayerName = Name;
+
+        }
+
+        private string getPlayerName()
+        {
+            return PlayerName;
+        }
+
+        private void setPlayerWallet(int Cash)
+        {
+            PlayerWallet = Cash;
+        }
+
+        private int getPlayerWallet()
+        {
+            return PlayerWallet;
+        }
+    }
+}
