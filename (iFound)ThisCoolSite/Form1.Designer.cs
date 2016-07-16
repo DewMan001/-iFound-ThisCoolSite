8960077741a4a6a0602b0df9c90c59fe3965311a
DewMan001 <deweymanster@gmail.com>
2016-07-16 20:01:00 +0800

    Player names laid out. Next up, scores and whatnot

--- (iFound)ThisCoolSite/Form1.Designer.cs
+++ (iFound)ThisCoolSite/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.lbl_Title = new System.Windows.Forms.Label();
+            this.lbl_Player1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Player2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_Title
@@ -41,11 +45,55 @@
             this.lbl_Title.TabIndex = 0;
             this.lbl_Title.Text = "CSGO GO Gadget Lottery!";
             // 
+            // lbl_Player1
+            // 
+            this.lbl_Player1.AutoSize = true;
+            this.lbl_Player1.Font = new System.Drawing.Font("Gill Sans MT", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Player1.Location = new System.Drawing.Point(13, 75);
+            this.lbl_Player1.Name = "lbl_Player1";
+            this.lbl_Player1.Size = new System.Drawing.Size(80, 31);
+            this.lbl_Player1.TabIndex = 1;
+            this.lbl_Player1.Text = "Player1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans MT", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Player3";
+            // 
+            // lbl_Player2
+            // 
+            this.lbl_Player2.AutoSize = true;
+            this.lbl_Player2.Font = new System.Drawing.Font("Gill Sans MT", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Player2.Location = new System.Drawing.Point(13, 126);
+            this.lbl_Player2.Name = "lbl_Player2";
+            this.lbl_Player2.Size = new System.Drawing.Size(80, 31);
+            this.lbl_Player2.TabIndex = 5;
+            this.lbl_Player2.Text = "Player2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 458);
+            this.ClientSize = new System.Drawing.Size(363, 458);
+            this.Controls.Add(this.lbl_Player2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Player1);
             this.Controls.Add(this.lbl_Title);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +105,10 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Label lbl_Player1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_Player2;
     }
 }
 
