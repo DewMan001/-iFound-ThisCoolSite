35eee8bdddc80f511dfbb4925a2a1d397cd89425
DewMan001 <deweymanster@gmail.com>
2016-07-26 21:29:07 +0800

    further work on edit forms

--- (iFound)ThisCoolSite/frm_Play1Edit.Designer.cs
+++ (iFound)ThisCoolSite/frm_Play1Edit.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.btn_Play1Save = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Play1Save
             // 
-            this.btn_Play1Save.Location = new System.Drawing.Point(607, 260);
+            this.btn_Play1Save.Location = new System.Drawing.Point(459, 195);
             this.btn_Play1Save.Name = "btn_Play1Save";
             this.btn_Play1Save.Size = new System.Drawing.Size(115, 47);
             this.btn_Play1Save.TabIndex = 0;
@@ -44,22 +46,41 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 200);
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 143);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(710, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(562, 26);
             this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wallet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 26);
+            this.textBox1.TabIndex = 3;
             // 
             // frm_Play1Edit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 319);
+            this.ClientSize = new System.Drawing.Size(586, 254);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.btn_Play1Save);
             this.Name = "frm_Play1Edit";
             this.Text = "frm_Play1Edit";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +88,7 @@
 
         private System.Windows.Forms.Button btn_Play1Save;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
